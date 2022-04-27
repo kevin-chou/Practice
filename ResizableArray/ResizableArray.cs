3c37d6c5ece2175586fb8469d1997d79ddadb70d
Kevin Chou <kevinchou-github@outlook.com>
2022-04-27 18:22:41 -0400

    Added code for a dynamic array

---
+++ ResizableArray/ResizableArray.cs
@@ -0,0 +1,125 @@
+﻿namespace ResizableArrayPractice
+{
+    /// <summary>
+    /// A an array which increases or decreases it's size dynamically to accomodate the number of elements in the array.
+    /// </summary>
+    public class ResizableArray<T>
+    {
+        private T[] resizableArray;
+        private uint count;         // # of elements in array
+
+        public ResizableArray()
+        {
+            count = 0;
+            resizableArray = new T[1];
+        }
+
+        public T this[int index]
+        {
+            get => resizableArray[index];
+        }
+
+        public int Length()
+        {
+            return resizableArray.Length;
+        }
+
+        /// <summary>
+        /// Adds an element to the end of the array.
+        /// </summary>
+        /// <param name="element">The value to be added.</param>
+        public void Append(T element)
+        {
+            // double array size if needed
+            if (count == resizableArray.Length)
+            {
+                AdjustArraySize(resizableArray.Length * 2);
+            }
+
+            // append element to array
+            resizableArray[count] = element;
+            count++;
+        }
+
+        /// <summary>
+        /// Removes the last element in the array.
+        /// </summary>
+        public void Delete()
+        {
+            if (count > 0 && !resizableArray[count - 1].Equals(default(T)))
+            {
+                resizableArray[count - 1] = default(T);
+                count--;
+
+                // decrease array size if needed
+                if (resizableArray.Length > 1 && count <= (resizableArray.Length / 2))
+                {
+                    AdjustArraySize(resizableArray.Length / 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes an element at the given index if it is not empty.
+        /// </summary>
+        /// <param name="index">The index in the array.</param>
+        public void RemoveAt(int index)
+        {
+            if (count > 0 && !resizableArray[index].Equals(default(T)))
+            {
+                resizableArray[index] = default(T);
+                count--;
+
+                // shift all elements to the left to fill empty space
+                for (int i = index; i < resizableArray.Length - 1; i++)
+                {
+                    resizableArray[i] = resizableArray[i + 1];
+                }
+
+                resizableArray[resizableArray.Length - 1] = default(T);
+
+                // decrease array size if needed
+                if (resizableArray.Length > 1 && count <= (resizableArray.Length / 2))
+                {
+                    AdjustArraySize(resizableArray.Length / 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adjusts the arrays current size.
+        /// </summary>
+        private void AdjustArraySize(int newSize)
+        {
+            // expand/shrink array to accomodate # of elements in existing array
+            T[] temp = new T[newSize];
+
+            // copy over existing values
+            for (int i = 0; i < count; i++)
+            {
+                temp[i] = resizableArray[i];
+            }
+
+            // update reference
+            resizableArray = temp;
+        }
+
+        /// <summary>
+        /// Prints out all elements in the array to console.
+        /// </summary>
+        public void Display()
+        {
+            Console.Write("Array: ");
+
+            for (int i = 0; i < resizableArray.Length; i++)
+            {
+                Console.Write(resizableArray[i]);
+
+                if (i + 1 < resizableArray.Length)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
---
+++ ResizableArray/Tester.cs
@@ -0,0 +1,77 @@
+﻿using ResizableArrayPractice;
+public class Tester
+{
+    // ----------------- Testing ----------------- 
+    public static void Main()
+    {
+        ResizableArray<int> myArray = new ResizableArray<int>();
+
+        // empty array
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // deleting from empty array
+        myArray.Delete();
+        myArray.RemoveAt(0);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // 1 element
+        myArray.Append(1);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // deleting from array with 1 element
+        myArray.Delete();
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // 2 elements
+        myArray.Append(1);
+        myArray.Append(2);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+        
+        // 3 elements + expand size to 4
+        myArray.Append(3);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+        
+        // 5 elements + expand size to 8
+        myArray.Append(4);
+        myArray.Append(5);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // 8 elements
+        myArray.Append(6);
+        myArray.Append(7);
+        myArray.Append(8);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // remove last element until first array shrink
+        myArray.Delete();
+        myArray.Delete();
+        myArray.Delete();
+        myArray.Delete();
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // remove elements from middle of array
+        myArray.Append(5);
+        myArray.Append(6);
+        myArray.Append(7);
+        myArray.Append(8);
+        myArray.RemoveAt(3);
+        myArray.RemoveAt(3);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+
+        // remove elements from beginning of array
+        myArray.RemoveAt(0);
+        myArray.RemoveAt(0);
+        myArray.Display();
+        Console.WriteLine("\n-------------------------------");
+    }
+}
