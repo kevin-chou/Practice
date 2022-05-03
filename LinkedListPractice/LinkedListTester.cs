4945366288ad55b7579827eb79e2823e62f3ae76
Kevin Chou <kevinchou-github@outlook.com>
2022-05-03 18:03:33 -0400

    Added implementation of linked list for practice

---
+++ LinkedListPractice/LinkedListTester.cs
@@ -0,0 +1,66 @@
+﻿namespace LinkedListPractice
+{
+    public class LinkedListTester
+    {
+        public static void Main()
+        {
+            var list = new SimpleLinkedList<string>();
+
+            // empty list
+            list.Display();
+
+            // prepend element to empty list
+            list.Prepend("Mug");
+            list.Display();
+
+            // delete head in list with only one element
+            list.Delete("Mug");
+
+            // list with one element
+            list.Append("Charlie");
+            list.Display();
+
+            // list with two elements
+            list.Append("Watchdog");
+            list.Display();
+
+            // list with 4 elements
+            list.Append("Birthday");
+            list.Append("Salamander");
+            list.Display();
+
+            // delete non existent element
+            list.Delete("Holiday");
+            list.Display();
+
+            // delete element in the middle ("Watchdog")
+            list.Delete("Watchdog");
+            list.Display();
+
+            // delete last element
+            list.Delete("Salamander");
+            list.Display();
+
+            // append elements after deleting last element
+            list.Append("Crazy");
+            list.Append("Keyboard");
+            list.Append("Johnny");
+            list.Display();
+
+            // delete multiple head elements
+            list.Delete("Charlie");
+            list.Delete("Birthday");
+            list.Display();
+
+            // delete until empty
+            list.Delete("Crazy");
+            list.Delete("Keyboard");
+            list.Delete("Johnny");
+            list.Display();
+
+            // add new element to new empty list
+            list.Append("Linked");
+            list.Display();
+        }
+    }
+}
---
+++ LinkedListPractice/SimpleLinkedList.cs
@@ -0,0 +1,170 @@
+﻿namespace LinkedListPractice
+{
+    /// <summary>
+    /// Practice implementation of a doubly linked list.
+    /// Outer class serves as a wrapper for the internal linked list node.
+    /// </summary>
+    public class SimpleLinkedList<T>
+    {
+        /// <summary>
+        /// Class representing a single node in a linked list.
+        /// </summary>
+        public class Node
+        {
+            private Node? next;
+            private Node? previous;
+            private readonly T data;
+
+            public T Data
+            {
+                get { return data; }
+            }
+
+            public Node? Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+            public Node? Previous
+            {
+                get { return previous; }
+                set { previous = value; }
+            }
+
+            public Node(T data)
+            {
+                next = null;
+                previous = null;
+                this.data = data;
+            }
+        }
+
+        private Node? head;
+
+        public SimpleLinkedList()
+        {
+            head = null;
+        }
+
+        /// <summary>
+        /// Adds a new element to the end of the linked list.
+        /// </summary>
+        /// <param name="data">The data to be added to linked list.</param>
+        public void Append(T data)
+        {
+            Node? curr = head;
+            Node node = new Node(data);
+
+            // linked list was empty
+            if (head == null)
+            {
+                head = node;
+                return;
+            }
+
+            // not empty, so traverse linked list and find the last node or "tail"
+            while (curr?.Next != null)
+            {
+                curr = curr.Next;
+            }
+
+            // attach or "link" the new node to the end of the list
+            curr.Next = node;
+            node.Previous = curr;
+        }
+
+        /// <summary>
+        /// Adds a new element to the beginning of the linked list.
+        /// </summary>
+        /// <param name="data">The data to be added to linked list.</param>
+        public void Prepend(T data)
+        {
+            // creating a node for the new head
+            Node node = new Node(data);
+            node.Next = head;
+
+            // update links from original (if it exists)
+            if (head != null)
+            {
+                head.Previous = node;
+            }
+
+            // assign the new node as the new head
+            head = node;
+        }
+
+        /// <summary>
+        /// Deletes the first element that contains the data.
+        /// </summary>
+        /// <param name="data">The data to be deleted.</param>
+        /// <returns>True if operation successful and false otherwise.</returns>
+        public bool Delete(T data)
+        {
+            // list is empty, no need to search
+            if (head == null)
+            {
+                return false;
+            }
+
+            // data is at first element, so we can easily delete it
+            // by moving head to next node
+            if (head.Data.Equals(data))
+            {
+                head = head.Next;
+
+                if (head != null && head.Next != null)
+                {
+                    head.Previous = null;
+                }
+                
+                return true;
+            }
+
+            // otherwise traverse list while searching for the data
+            Node? curr = head;
+
+            while(curr != null)
+            { 
+                if (curr.Data != null && curr.Data.Equals(data))
+                {
+                    // found match, delete it from list by updating links
+                    curr.Previous.Next = curr.Next;
+
+                    if (curr.Next != null)
+                    {
+                        curr.Next.Previous = curr.Previous;
+                    }
+
+                    return true;
+                }
+
+                curr = curr.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Prints out a visual representation of the linked list to console.
+        /// </summary>
+        public void Display()
+        {
+            Node? curr = head;
+
+            while (curr != null)
+            {
+                Console.Write("[ " + curr.Data.ToString() + " ]");
+
+                if (curr.Next != null)
+                {
+                    Console.Write(" - ");
+                }
+
+                curr = curr.Next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
